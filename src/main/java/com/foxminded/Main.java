1ff6c7cff5f07039cba4fb87cc75e4fa4a0a7a26
Nazarii Hubych <hubychnazarii@gmail.com>
2019-08-14 21:34:58 +0300

    removed unnecessary folders

--- src/main/java/com/foxminded/Main.java
+++
@@ -1,8 +0,0 @@
-package main.java.com.foxminded;
-
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.print(new PrintDivision().printIntegerDivision(78945, 4));
-    }
-}
